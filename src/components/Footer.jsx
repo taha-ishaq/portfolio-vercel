85e8b078db35160b2da5bf6ea97388c01c802f30
taha-ishaq <145692418+taha-ishaq@users.noreply.github.com>
2023-10-02 13:43:36 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white'>
         <div className='container p-12  flex justify-between'>
-            <span>LOGO</span>
+            <span><img className='rounded-half flex' width={50} height={50} src="\images\i.png" alt="" /></span>
             <p className='text-slate-600'>All Rights Reserved</p>
             </div>
             </footer>
