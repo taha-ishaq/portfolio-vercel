d2b70a7eda65efd29b99d0a1fdd3eb49b0e9d10a
taha-ishaq <145692418+taha-ishaq@users.noreply.github.com>
2023-10-02 15:03:45 -0700

    Update EmailSection.jsx

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -1,38 +1,26 @@
 "use client";
-import React ,{useState} from 'react'
+import React ,{useState ,useRef} from 'react'
 import GithubIcon from "../../public/github-icon.svg"
 import LinkedinIcon  from "../../public/linkedin-icon.svg";
 import Link from 'next/link';
 import Image from 'next/image';
+import emailjs from '@emailjs/browser';
 
 const EmailSection = () => {
-    const [emailSubmitted, setEmailSubmitted] =useState(false);
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-        const data ={
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint ="/api/send";
+    const [emailSubmitted, setEmailSubmitted] = useState(false)
+    const form = useRef( );
+   
 
-        const options ={
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json',
-
-            },
-            body:JSONdata,
-        }
-        const response = await fetch(endpoint,options);
-        const resData =await response.json();
-        console.log(resData);
-        
-        if(response.status === 200){
-            console.log("message sent")
-            setEmailSubmitted(true);
-        }
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_tsdcjqd', 'template_vj8ef3c', form.current, 'qf5k6VlZKIaWcLneM')
+        .then((result) => {
+            console.log(result.text);
+            setEmailSubmitted(true)
+        }, (error) => {
+            console.log(error.text);
+        });
     }
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
@@ -53,32 +41,27 @@ const EmailSection = () => {
         </div>
         </div>
         <div>
-            <form className='flex flex-col' onSubmit={handleSubmit}>
+            <form ref={form} className='flex flex-col' onSubmit={sendEmail}>
                 <div className='mb-6'>
-                <label htmlFor='email' className='text-white block mb-2  text-sm font-medium '>Your Email</label>
-                <input name='email' className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' type='email' id='email' required placeholder='Taha@gmail.com' /> 
+                <label  className='text-white block mb-2  text-sm font-medium '>Your Email</label>
+                <input type="email" name='user_email' className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' required placeholder='Taha@gmail.com' /> 
                 </div>
                 <div className='mb-6'>
-                <label htmlFor='subject' className='text-white block mb-2 text-sm font-medium '>Subject</label>
-                <input name='subject'  className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' type='text' id='subject' required placeholder='Just saying Hi' /> 
+                <label  className='text-white block mb-2 text-sm font-medium '>Name</label>
+                <input type="text" name='user_name'  className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' required placeholder='Just saying Hi' /> 
                 </div>
                 <div className='mb-6'>
-                    <label htmlFor='message' className='text-white block text-sm mb-2 font-medium'>Message</label>
-                    <textarea name="message" id="message" className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Leave your Message Here'></textarea>
+                    <label className='text-white block text-sm mb-2 font-medium'>Message</label>
+                    <textarea name='message' id="message" className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Leave your Message Here'></textarea>
                 </div>
-                <button type='submit'  className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message
+                <button type='submit' value="Send"  className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message
                 </button>
-                {
-                    emailSubmitted && (
-                        <p className='text-green-500 text-sm mt-2'>
-                            Email sent Successfully!
-                        </p>
-                    )
-                }
+               
             </form>
+            {emailSubmitted && <p className='text-green-500 text-sm mt-2'>Email sent</p>}
         </div>
     </section>
   )
-}
+            }
 
 export default EmailSection
