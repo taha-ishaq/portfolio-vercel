314145683fe88bc1999e02649a210e2cedc39eee
taha-ishaq <145692418+taha-ishaq@users.noreply.github.com>
2023-10-02 15:13:58 -0700

    Update EmailSection.jsx

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -4,6 +4,7 @@ import GithubIcon from "../../public/github-icon.svg"
 import LinkedinIcon  from "../../public/linkedin-icon.svg";
 import Link from 'next/link';
 import Image from 'next/image';
+import emailjs from '@emailjs/browser';
 
 const EmailSection = () => {
    
