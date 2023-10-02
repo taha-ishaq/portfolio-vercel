dfedb9c97b1034739bc1f99d73901d0574b672fd
taha-ishaq <145692418+taha-ishaq@users.noreply.github.com>
2023-10-02 15:06:20 -0700

    Update EmailSection.jsx

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -1,26 +1,38 @@
 "use client";
-import React ,{useState ,useRef} from 'react'
+import React ,{useState} from 'react'
 import GithubIcon from "../../public/github-icon.svg"
 import LinkedinIcon  from "../../public/linkedin-icon.svg";
 import Link from 'next/link';
 import Image from 'next/image';
-import emailjs from '@emailjs/browser';
 
 const EmailSection = () => {
-    const [emailSubmitted, setEmailSubmitted] = useState(false)
-    const form = useRef( );
-   
+    const [emailSubmitted, setEmailSubmitted] =useState(false);
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        const data ={
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        };
+        const JSONdata = JSON.stringify(data);
+        const endpoint ="/api/send";
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_tsdcjqd', 'template_vj8ef3c', form.current, 'qf5k6VlZKIaWcLneM')
-        .then((result) => {
-            console.log(result.text);
-            setEmailSubmitted(true)
-        }, (error) => {
-            console.log(error.text);
-        });
+        const options ={
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+
+            },
+            body:JSONdata,
+        }
+        const response = await fetch(endpoint,options);
+        const resData =await response.json();
+        console.log(resData);
+        
+        if(response.status === 200){
+            console.log("message sent")
+            setEmailSubmitted(true);
+        }
     }
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
@@ -31,37 +43,42 @@ const EmailSection = () => {
             {" "}I'm currently Looking for new opportunities , my inbox is always open , whether you have a question  or just  want to say hi , i'll try my best  to get back to you!
         </p>
         <div  className='socials flex flex-row gap-2'>
-            <Link href="https://github.com/taha-ishaq">
+            <Link href="github.com">
                 <Image src={GithubIcon} alt='Github Icon' />
             </Link>
             
-            <Link href="https://www.linkedin.com/in/taha-ishaq-18048528a/">
+            <Link href="linkedin.com">
                 <Image src={LinkedinIcon} alt='linkedin Icon' />
             </Link>
         </div>
         </div>
         <div>
-            <form ref={form} className='flex flex-col' onSubmit={sendEmail}>
+            <form className='flex flex-col' onSubmit={handleSubmit}>
                 <div className='mb-6'>
-                <label  className='text-white block mb-2  text-sm font-medium '>Your Email</label>
-                <input type="email" name='user_email' className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' required placeholder='Taha@gmail.com' /> 
+                <label htmlFor='email' className='text-white block mb-2  text-sm font-medium '>Your Email</label>
+                <input name='email' className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' type='email' id='email' required placeholder='Taha@gmail.com' /> 
                 </div>
                 <div className='mb-6'>
-                <label  className='text-white block mb-2 text-sm font-medium '>Name</label>
-                <input type="text" name='user_name'  className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' required placeholder='Just saying Hi' /> 
+                <label htmlFor='subject' className='text-white block mb-2 text-sm font-medium '>Subject</label>
+                <input name='subject'  className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' type='text' id='subject' required placeholder='Just saying Hi' /> 
                 </div>
                 <div className='mb-6'>
-                    <label className='text-white block text-sm mb-2 font-medium'>Message</label>
-                    <textarea name='message' id="message" className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Leave your Message Here'></textarea>
+                    <label htmlFor='message' className='text-white block text-sm mb-2 font-medium'>Message</label>
+                    <textarea name="message" id="message" className='bg-[#18191E] border border-[#33353F] placeholde-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Leave your Message Here'></textarea>
                 </div>
-                <button type='submit' value="Send"  className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message
+                <button type='submit'  className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message
                 </button>
-               
+                {
+                    emailSubmitted && (
+                        <p className='text-green-500 text-sm mt-2'>
+                            Email sent Successfully!
+                        </p>
+                    )
+                }
             </form>
-            {emailSubmitted && <p className='text-green-500 text-sm mt-2'>Email sent</p>}
         </div>
     </section>
   )
-            }
+}
 
 export default EmailSection
