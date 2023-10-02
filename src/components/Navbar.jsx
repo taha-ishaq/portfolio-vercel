cd5f38a8a75964cd39ad6f8af79acbd467c03f1f
taha-ishaq <145692418+taha-ishaq@users.noreply.github.com>
2023-10-02 13:44:45 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
-        <Link href={"/"} className='text-2xl  md:text-5xl text-white font-semibold'>Logo</Link>
+        <Link href={"/"} className='text-2xl  md:text-5xl text-white font-semibold'> <img width={50} height={70} src='\images\i.png' /></Link>
         <div className=' mobile -menu block md:hidden '>
            {navbarOpen ?(
                 <button onClick={()=> setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200  text-slate-200 hover:text-white hover:border-white'><XMarkIcon className='h-5 w-5' />
